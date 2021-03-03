12e57ac31af2ec3620565d2cda133e502bd9220c
Michal Balicki <stiekerosiem@gmail.com>
2021-03-03 21:42:22 +0100

    add categories manager

---
+++ frontend/actions/category.js
@@ -0,0 +1,52 @@
+import fetch from "isomorphic-fetch";
+
+export const create = (category, token) => {
+  return fetch(`${process.env.NEXT_PUBLIC_API}/category`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const getCategories = () => {
+  return fetch(`${process.env.NEXT_PUBLIC_API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const singleCategory = (slug) => {
+  return fetch(`${process.env.NEXT_PUBLIC_API}/category/${slug}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const removeCategory = (slug, token) => {
+  return fetch(`${process.env.NEXT_PUBLIC_API}/category/${slug}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.error(err));
+};
