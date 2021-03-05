3521172dcdbe37c36309b8370e7c1a38e8598ce8
Michal Balicki <stiekerosiem@gmail.com>
2021-03-05 12:34:19 +0100

    add blog endpoint

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -1,3 +1,109 @@
-exports.time = (req, res) => {
-    res.json({time: Date().toString});
+// models
+const Blog = require("../models/blog");
+
+// imports
+const formidable = require("formidable");
+const slugify = require("slugify");
+const { stripHtml } = require("string-strip-html");
+const _ = require("lodash");
+
+// helpers
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const { smartTrim } = require("../helpers/blog");
+
+// JS
+const fs = require("fs");
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not upload.",
+      });
+    }
+
+    const { title, body, categories, tags } = fields;
+
+    if (!title || !title.length) {
+      return res.status(400).json({
+        error: "Title is required.",
+      });
+    }
+
+    if (!body || body.length < 200) {
+      return res.status(400).json({
+        error: "Content is too short.",
+      });
+    }
+
+    if (!categories || categories.length === 0) {
+      return res.status(400).json({
+        error: "At least one category is required.",
+      });
+    }
+
+    if (!tags || tags.length === 0) {
+      return res.status(400).json({
+        error: "At least one tag is required.",
+      });
+    }
+
+    let blog = new Blog();
+    blog.title = title;
+    blog.body = body;
+    blog.excerpt = smartTrim(body, 320, " ", "...");
+    blog.slug = slugify(title).toLowerCase();
+    blog.mtitle = `${title} - ${process.env.APP_NAME}`;
+    blog.mdesc = stripHtml(body.substr(0, 160)).result;
+    blog.postedBy = req.user._id;
+
+    let arrayOfCategories = categories && categories.split(",");
+    let arrayOfTags = tags && tags.split(",");
+
+    if (files.photo) {
+      if (files.photo.size > 10000000) {
+        return res.status(400).json({
+          error: "Image size is too big. Max size is 1mb.",
+        });
+      }
+
+      blog.photo.data = fs.readFileSync(files.photo.path);
+      blog.photo.contentType = files.photo.type;
+    }
+
+    blog.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      Blog.findByIdAndUpdate(
+        result._id,
+        { $push: { categories: arrayOfCategories } },
+        { new: true }
+      ).exec((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        } else {
+          Blog.findByIdAndUpdate(
+            result._id,
+            { $push: { tags: arrayOfTags } },
+            { new: true }
+          ).exec((err, result) => {
+            if (err) {
+              return res.status(400).json({
+                error: errorHandler(err),
+              });
+            } else {
+              res.json(result);
+            }
+          });
+        }
+      });
+    });
+  });
 };
