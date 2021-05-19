2eb99ffd633bf6a2d2d48b40fb39d3bd5ecb85a4
Michal Balicki <stiekerosiem@gmail.com>
2021-05-19 17:21:30 +0200

    add pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
 const nextTranslate = require("next-translate");
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = {
+module.exports = withPWA({
   ...nextTranslate(),
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
   images: {
     domains: [process.env.NEXT_PUBLIC_DOMAIN],
   },
   future: {
     webpack5: true,
   },
-};
+});
