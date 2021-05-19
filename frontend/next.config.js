3feda14169e7ccfa7f187fdc56d07499c694bf1f
Michal Balicki <stiekerosiem@gmail.com>
2021-05-19 22:58:58 +0200

    turn off pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,18 @@
 const nextTranslate = require("next-translate");
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require("next-pwa");
+// const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+// module.exports = withPWA({
+module.exports = {
   ...nextTranslate(),
-  pwa: {
-    dest: "public",
-    runtimeCaching,
-  },
+  // pwa: {
+  //   dest: "public",
+  //   runtimeCaching,
+  // },
   images: {
     domains: [process.env.NEXT_PUBLIC_DOMAIN],
   },
   future: {
     webpack5: true,
   },
-});
+};
